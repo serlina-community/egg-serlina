7902303dadff56635ed7143bd807fe014a8b1c96
Randy <randypriv@gmail.com>
2018-08-10 17:08:06 +0800

    await next() finally

--- app/middleware/serlina-route.js
+++ app/middleware/serlina-route.js
@@ -3,11 +3,13 @@
 module.exports = () => {
   return async function serlinaRoute(ctx, next) {
     const config = ctx.app.config.serlina;
-    if (config.map && config.map[ctx.path]) {
-      const rendered = await ctx.app.serlina.render(config.map[ctx.path], { ctx });
-      ctx.body = rendered.string;
+    try {
+      if (config.map && config.map[ctx.path]) {
+        const rendered = await ctx.app.serlina.render(config.map[ctx.path], { ctx });
+        ctx.body = rendered.string;
+      }
+    } finally {
+      await next();
     }
-
-    await next();
   };
 };
